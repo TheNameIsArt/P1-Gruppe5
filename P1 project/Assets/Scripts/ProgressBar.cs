0c1608492617be695bf6b766f0d077cba5a6e5c6
Viktor <viktor.bergstroem@hotmail.com>
2024-11-05 10:08:36 +0100

    Start på en progress bar

---
+++ P1 project/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ProgressBar : MonoBehaviour
+{
+    private Slider progressBar; 
+    private float fillSpeed = 0.04f; //Controls the speed of the bar 
+    private float targetProgress = 0; 
+
+    private void Awake()
+    {
+        progressBar = gameObject.GetComponent<Slider>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        IncrementProgress(1f); // How much of the slider is filled 
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (progressBar.value < targetProgress)
+        progressBar.value += fillSpeed * Time.deltaTime;
+    }
+
+    // Add progress to the bar
+    public void IncrementProgress(float newProgress)
+    {
+        targetProgress = progressBar.value + newProgress;
+    }
+}
