6bdb23964be2afbc7d9d9b23f01d2dab0409f8ee
ChristopherSHedegaard <chrissh02@gmail.com>
2024-11-18 09:19:24 +0100

    Vores Arrays

---
+++ P1 project/Assets/Scripts/WordGenerator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.Collections.LowLevel.Unsafe;
+using System;
+using System.Globalization;
+
+public class WordGenerator : MonoBehaviour
+{
+    string[] DanokWords = { "labi", "laci",
+        "ladi", "lafi", "lagi", "lahi", "laji","laki",
+        "lali", "lami", "lani", "lapi", "laqi", "lari",
+        "lasi", "lati", "lavi", "lawi", "laxi", "lazi"};
+
+    string[] Consonants = {"b", "c", "d", "f", "g", "h",
+        "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"};
+
+    void Start()
+    {
+    
+
+    }
+ 
+}
+  
+
+  
+
