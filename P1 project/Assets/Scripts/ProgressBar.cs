abf36a6d610248dc337a9efc94d0d92feaddb532
Viktor <viktor.bergstroem@hotmail.com>
2024-11-19 12:13:53 +0100

    Progress bar update
    Progress baren er opdateret og virker-ish!

--- P1 project/Assets/Scripts/ProgressBar.cs
+++ P1 project/Assets/Scripts/ProgressBar.cs
@@ -2,27 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 public class ProgressBar : MonoBehaviour
 {
     private Slider progressBar; 
     private float fillSpeed = 0.04f; //Controls the speed of the bar 
     private float targetProgress = 0; 
+    private bool isPaused = false; //Controls when the bar is paused
+    public Button Cons_1Button;
+
+    private List<float> pausePoints = new List<float> {0.25f, 0.5f, 0.75f}; //Pause points
+    
 
     private void Awake()
     {
         progressBar = gameObject.GetComponent<Slider>();
     }
-    // Start is called before the first frame update
+    
     void Start()
     {
         IncrementProgress(1f); // How much of the slider is filled 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        if (progressBar.value < targetProgress)
-        progressBar.value += fillSpeed * Time.deltaTime;
+        // Update the progress bar only if not paused
+        if (!isPaused && progressBar.value < targetProgress)
+        {
+            progressBar.value += fillSpeed * Time.deltaTime;
+
+            // Check if we hit a pause point
+            foreach (float pausePoint in pausePoints)
+            {
+                if (progressBar.value >= pausePoint && progressBar.value < pausePoint + fillSpeed * Time.deltaTime)
+                {
+                    PauseProgress();
+                    break;
+                }
+            }
+        }
     }
 
     // Add progress to the bar
@@ -30,4 +50,19 @@ public class ProgressBar : MonoBehaviour
     {
         targetProgress = progressBar.value + newProgress;
     }
+    
+    // Pause the bar
+    public void PauseProgress()
+    {
+        isPaused = true;
+        Cons_1Button.gameObject.SetActive(true);
+    }
+
+    // Resume the bar 
+    public void ResumeProgress()
+    {
+        isPaused = false;
+        Cons_1Button.gameObject.SetActive(false);
+    }
+
 }
