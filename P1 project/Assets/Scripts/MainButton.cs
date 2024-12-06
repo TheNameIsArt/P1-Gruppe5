8c070e9312774d180a8f818de78131dd9ad28b65
TheNameIsArt <62926983+TheNameIsArt@users.noreply.github.com>
2024-12-06 14:36:53 +0100

    knapper

--- P1 project/Assets/Scripts/MainButton.cs
+++ P1 project/Assets/Scripts/MainButton.cs
@@ -3,8 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class MainButton : MonoBehaviour
 {
+    public void LoadMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
     public void LoadGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene(2);
     }
 }
