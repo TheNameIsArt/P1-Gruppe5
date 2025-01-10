dafdb5759a239054fac487f83985957595fddb70
Marc5715 <marcus@juulnielsen.dk>
2025-01-10 14:10:00 +0100

    Removed Lane 4

--- P1 project/Assets/Scripts/Runner scripts/LaneMover.cs
+++ P1 project/Assets/Scripts/Runner scripts/LaneMover.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class LaneMover : MonoBehaviour
 {
     public int laneNumber = 1; //players starting lane
-    private float[] laneYPositions = {-5f, -3.5f, -1.5f, 2f}; //Array of y-positions for lanes. Also determins amount of lanes
+    private float[] laneYPositions = {-5f, -3.5f, -1.5f}; //Array of y-positions for lanes. Also determins amount of lanes
 
     public float LateralLaneMoveSpeed = 10f; //speed when lane-switching
 
